dbb090ad22dfc6ab7c1f1834a13c706ee9dada30
Ghanshyam Singh <ghan6171@gmail.com>
2023-04-07 11:52:58 +0530

    added edit and delete views

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+
 app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('new_post', views.new_post, name='new-post'),
     path('search/', views.search, name='search'),
-    path('<slug:profile>/', views.profile, name='profile'),
+    path('<slug:profile>', views.profile, name='profile'),
     path('<slug:author>/<slug:slug>/', views.detail_post, name='detail'),
+    path('<slug:author>/<slug:slug>/edit', views.edit_post, name='edit-post'),
+    path('<slug:author>/<slug:slug>/delete-confirm', views.delete_post, name='delete-post'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponseRedirect, Http404
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.urls import reverse
 from core.models import Post
 from django.db.models import Q
@@ -21,6 +21,7 @@ def detail_post(request, author, slug):
     }
     return render(request, 'core/detail.html', context)
 
+
 @login_required
 def new_post(request):    
     if request.method == 'POST':
@@ -36,13 +37,51 @@ def new_post(request):
         return render(request, 'core/newpost.html', context)
 
 
+@login_required
+def edit_post(request, author, slug):
+    """Edit post view in which we take post object and send it to the form instance."""
+    
+    post = get_object_or_404(Post, author__username=author, slug=slug)
+    
+    # Here, we are verifying if the author == logged in user, if yes then
+    # send to edit post or else raise a 404 error.
+    if author != request.user.username:
+        raise Http404
+        
+    if request.method != 'POST':
+        form = NewPostForm(instance=post)
+    else:
+        form = NewPostForm(instance=post, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(reverse('core:detail', args=[request.user.username, post.slug]))
+    
+    context = {'post': post, 'form': form}
+    return render(request, 'core/edit.html', context)        
+
+
+@login_required
+def delete_post(request, author, slug):
+    post = get_object_or_404(Post, author__username=author, slug=slug)
+    if request.user != post.author:
+        raise Http404
+    
+    if request.method == 'POST':
+        post.delete()
+        return redirect(reverse('core:index'))
+    return render(request, 'core/delete.html', {'post': post})
+        
+
 def search(request):
     results = []
     if request.method == 'GET':
         query = request.GET.get('q')
         
-    results = Post.objects.filter(Q(title__icontains=query), status=1).order_by('-created_on')
-    return render(request, 'core/search.html', {'object_list': results, 'query': query})
+    results = Post.objects.filter(Q(title__icontains=query) | Q(), status=1).order_by('-created_on')
+    context = {
+        'object_list': results, 'query': quer
+    }
+    return render(request, 'core/search.html', context)
         
         
 def profile(request, profile):
